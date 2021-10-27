8cd0799a1c00a575a7de56da57819a0da947e766
nchrist5 <nchrist5@emich.edu>
2021-10-26 21:57:00 -0400

    movement design

---
+++ Assets/Scripts/Pivot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pivot : MonoBehaviour
+{
+    public GameObject crosshair;
+    public GameObject player;
+    private Vector3 target;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshair.transform.position = new Vector2(target.x, target.y);
+
+
+        Vector3 difference = target - player.transform.position;
+        difference.Normalize();
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg - 90;
+        player.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+    
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public Rigidbody2D rb;
+    public Vector2 movement;
+    public float maxSpeed = 1f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(0, Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        movePlayer(movement);
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+
+    void movePlayer(Vector2 direction) 
+    {
+        rb.AddRelativeForce(direction * speed);
+    }
+}
