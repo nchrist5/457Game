f952d7aa074d89089c29059c5213e4b83697fa37
nchrist5 <nchrist5@emich.edu>
2021-12-02 14:19:58 -0500

    Added upgraded default weapon

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -11,6 +11,12 @@ public class PlayerWeapon : MonoBehaviour
     public float playerFireRate = 0.2f;
     private float playerNextFire = 0.0f;
 
+    public static bool defaultWeapon = true;
+    public static bool upgrade = false;
+    public static bool railgun = false;
+    public static bool shotgun = false;
+    public static bool grenade = false;
+
     // Update is called once per frame
     void Update()
     {
@@ -18,7 +24,14 @@ public class PlayerWeapon : MonoBehaviour
         if ((Input.GetButton("Fire1")) && (Time.time > playerNextFire))
         {
             playerNextFire = Time.time + playerFireRate;
-            Shoot();
+
+            //check which weapon is equipped
+            if (upgrade == true) {
+                ShootUpgrade();
+            } else
+            {
+                Shoot();
+            }   
         }
     }
 
@@ -29,5 +42,17 @@ public class PlayerWeapon : MonoBehaviour
         GameObject laser = Instantiate(LaserProjectile, playerFirePoint.position, playerFirePoint.rotation);
         Rigidbody2D rb = laser.GetComponent<Rigidbody2D>();
         rb.AddForce(playerFirePoint.up * laser_speed, ForceMode2D.Impulse);
+    }
+
+    //shooting method for upgraded laser
+    void ShootUpgrade()
+    {
+        GameObject laser1 = Instantiate(LaserProjectile, playerFirePoint.position + new Vector3(-0.3f, 0.0f, 0.0f), playerFirePoint.rotation);
+        GameObject laser2 = Instantiate(LaserProjectile, playerFirePoint.position + new Vector3(0.3f, 0.0f, 0.0f), playerFirePoint.rotation);
+
+        Rigidbody2D rb1 = laser1.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb2 = laser2.GetComponent<Rigidbody2D>();
+        rb1.AddForce(playerFirePoint.up * laser_speed, ForceMode2D.Impulse);
+        rb2.AddForce(playerFirePoint.up * laser_speed, ForceMode2D.Impulse);
     }
 }
