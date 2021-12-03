160cec21d4877428e485cb6e4f15ae686c6cb311
nchrist5 <nchrist5@emich.edu>
2021-12-03 18:43:28 -0500

    Added extended music track. Set default weapon to true. Added passive menus to build settings

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -18,9 +18,9 @@ public class PlayerWeapon : MonoBehaviour
 
     private float playerNextFire = 0.0f;
 
-    public static bool defaultWeapon = false;
+    public static bool defaultWeapon = true;
     public static bool upgrade = false;
-    public static bool railgun = true;
+    public static bool railgun = false;
     public static bool shotgun = false;
     public static bool grenade = false;
 
