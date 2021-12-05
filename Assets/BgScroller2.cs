eec2794b4d1d5769974f7b7f8756e287ec16b78e
dchraim <dchraim@emich.edu>
2021-12-05 04:03:26 -0500

    added moving background to level 1

---
+++ Assets/BgScroller2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroller2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+ transform.position += new Vector3(-2 * Time.deltaTime, 0);
+
+if (transform.position.x < -14.95)
+     {
+         transform.position = new Vector3(14.95f, transform.position.y);
+         
+     }
+     
+    }
+}
