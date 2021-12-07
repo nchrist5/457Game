d47e21dd6c95c45132b05c31bbc22104a93da604
nchrist5 <nchrist5@emich.edu>
2021-12-07 15:30:31 -0500

    Added level indicators. Adjusted background on level1. Tweaked boss fight

---
+++ Assets/Scripts/LevelIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelIndicator : MonoBehaviour
+{
+    public Text indicator;
+    public Text mainIndicator;
+    private float showIndicator = 2.5f;
+    private float endIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainIndicator.enabled = false;
+        indicator.enabled = true;
+        endIndicator = Time.time + showIndicator;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (indicator.enabled & Time.time > endIndicator)
+        {
+            indicator.enabled = false;
+            mainIndicator.enabled = true;
+        }
+    }
+}
